abf60e328f531602d90615d3b115aa8cec0eaae1
Marion <marion.lebras@nist.gov>
2018-04-07 17:53:44 -0400

    More updates to init__.py's to support easy submoduling

--- d3m_outputs/__init__.py
+++ d3m_outputs/__init__.py
@@ -1,5 +1,7 @@
 __all__ = ['predictions', 'file_checker', 'metrics',
-           'pipeline_logs_validator', 'schemas', 'validation_type_checks']
+           'pipeline_logs_validator', 'schemas', 'validation_type_checks',
+           'is_predictions_file_valid', 'score_predictions_file', 'Predictions',
+           'PipelineLog']
 
 from .predictions import is_predictions_file_valid, score_predictions_file, Predictions
 from .pipeline_logs_validator import PipelineLog
