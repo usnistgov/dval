500416bce48e019e2ad95bc1ac86e2c08a9a88d9
Marion <marion.lebras@nist.gov>
2018-06-01 18:34:21 -0400

    rest of the partial commit. validation for old pipelines and new bare.
    TODO: cli integration and clean up of old code.

---
+++ CHANGELOG.md
@@ -0,0 +1,21 @@
+## vNEXT
+First release for *Summer 2018 Evaluation* Validation and Scoring
+
+* Add a CHANGELOG.md file tracking version changes.
+* Support for MIT-LL data specification v3.1
+* Support for v3.1 metrics and metric parameters
+  * f1 parameter
+  * score on all targets
+* Support for metalearning format pipelines
+* Full Task 2 outputs validation - summer 2018 specification
+* Task 2 scoring - summer 2018 specification
+* Only requires problem schema for task 2 validation, previously required full data structure. Backwards compatible 
+
+
+## v2018.4.28
+Last release according to *February 2018* Evaluation standards.
+
+Released on April 4th 2018
+
+* Added post-search validation
+* Added test script generation
---
+++ test/test_pipeline_validation.py
@@ -0,0 +1,34 @@
+from glob import glob
+from d3m_outputs.pipeline_logs_validator import load_json, is_pipeline_valid_old_schema, is_pipeline_valid_bare
+
+def test_old_pipeline_pass():
+    files = glob('test/pipelines/old_format/pass/*.json')
+    assert len(files) > 0
+    for file in files:
+        jpipeline = load_json(file)
+        assert is_pipeline_valid_old_schema(jpipeline) == True
+
+
+def test_old_pipeline_fail():
+    files = glob('test/pipelines/old_format/fail/*.json')
+    assert len(files) > 0
+    for file in files:
+        jpipeline = load_json(file)
+        assert is_pipeline_valid_old_schema(jpipeline) == False
+
+
+def test_bare_pipeline_pass():
+    files = glob('test/pipelines/new_format_bare/pass/*.json')
+    assert len(files) > 0
+    for file in files:
+        jpipeline = load_json(file)
+        assert is_pipeline_valid_bare(jpipeline) == True
+
+
+def test_bare_pipeline_fail():
+    files = glob('test/pipelines/new_format_bare/fail/*.json')
+    assert len(files) > 0
+    for file in files:
+        jpipeline = load_json(file)
+        assert is_pipeline_valid_old_schema(jpipeline) == False
+
