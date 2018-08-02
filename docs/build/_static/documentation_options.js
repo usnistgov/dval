54397eba3f2c8ba88510db19218f59407bdffdff
Peter Fontana <peter.fontana@nist.gov>
2018-08-02 17:16:38 -0400

    Added sphinx docs and sphinx doc generation with autodoc to produce html docs.

---
+++ docs/build/_static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: '18summer-1.4-alpha',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
