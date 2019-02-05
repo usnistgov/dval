e0c6103a900e450372e70db934c4461adde49557
anb30 <alexandre.boyer@nist.gov>
2019-02-05 13:59:44 -0500

    Update __init__.py

--- d3m_outputs/__init__.py
+++ d3m_outputs/__init__.py
@@ -5,7 +5,7 @@ __all__ = ['predictions', 'file_checker', 'metrics', 'generated_problems',
            'is_pipeline_valid', 'is_pipeline_valid_old_schema',
            'is_pipeline_valid_full_validation', 'is_pipeline_valid_bare']
 
-__version__ = '18summer-1.5-dev3'
+__version__ = '18summer-1.5-dev4'
 
 from .predictions import is_predictions_file_valid, score_predictions_file, Predictions
 from .pipeline_logs_validator import is_pipeline_valid, is_pipeline_valid_bare, \
