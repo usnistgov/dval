36cac707b30675e9d8a25c8d03fe898904d011ca
Marion <marion.le-bras@esial.net>
2019-02-13 12:13:20 -0500

    Trying to address docs job failing by adding source to PATH

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ import os
 import datetime
 import sys
 
-# sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath("../.."))
 
 # Get configuration information from setup.cfg
 try:
